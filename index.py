91551fdb21c1bd9b543177d998d8fa20c943c89c
Dtttttttttt <dt0918274@gmail.com>
2023-10-24 13:46:05 +0800

    v1

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ def index():
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=王琮善>傳送使用者暱稱</a><br>"
     homepage += "<a href=/about>琮善簡介網頁</a><br>"
+    homepage += "<a href=/account>帳號密碼</a><br>"
     return homepage
 
 
